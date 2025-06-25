6dff92bc7fddfd2f32be0bc682afe9481223d157
Alexandra Vaca <107285163+alwxav98@users.noreply.github.com>
2025-06-24 22:41:31 -0500

    Update estacion.py

--- models/estacion.py
+++ models/estacion.py
@@ -1,11 +1,19 @@
+from shapely.geometry import MultiPoint
 class Sensor:
-    def __init__(self, codigo, nombre, tipo, latitud, longitud, variable):
+    def __init__(self, codigo, nombre, tipo, latitud, longitud, variable,
+                 contexto_geoambiental=None, reserva=None, fuente_hidrica=None,
+                 ecosistema=None, influencia=None):
         self.codigo = codigo
         self.nombre = nombre
         self.tipo = tipo
         self.latitud = latitud
         self.longitud = longitud
         self.variable = variable
+        self.contexto_geoambiental = contexto_geoambiental
+        self.reserva = reserva
+        self.fuente_hidrica = fuente_hidrica
+        self.ecosistema = ecosistema
+        self.influencia = influencia
 
     def to_dict(self):
         return {
@@ -14,8 +22,14 @@ class Sensor:
             "tipo": self.tipo,
             "latitud": self.latitud,
             "longitud": self.longitud,
-            "variable": self.variable
+            "variable": self.variable,
+            "contexto_geoambiental": self.contexto_geoambiental,
+            "reserva": self.reserva,
+            "fuente_hidrica": self.fuente_hidrica,
+            "ecosistema": self.ecosistema,
+            "influencia": self.influencia
         }
+
 
 
 class EstacionManager:
@@ -33,3 +47,9 @@ class EstacionManager:
 
     def todos_dict(self):
         return [s.to_dict() for s in self.sensores]
+
+    def zona_envolvente(self):
+        puntos = [(s.latitud, s.longitud) for s in self.sensores]
+        multipunto = MultiPoint(puntos)
+        poligono = multipunto.convex_hull
+        return list(poligono.exterior.coords)
